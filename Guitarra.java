ea89d08a1d3285c301c8730549b66321ef8348f9
Fernanda F. Fonseca <65093157+ffer23@users.noreply.github.com>
2020-05-09 22:36:37 -0300

    Create Guitarra.java

---
+++ Guitarra.java
@@ -0,0 +1,75 @@
+package unidade_1;
+
+////declaração da classe
+
+      private string numeroSerie, fabricante, modelo, tipo, madeira;
+      private double preço;
+
+////criação do construtor da classe guitarra
+
+  public Guitarra(
+              string numeroSerie, string fabricante,
+              string modelo, string tipo,
+              string madeira, double preço) {
+         this.numeroSerie = numeroSerie;
+         this.fabricante = fabricante;
+         this.modelo = modelo;
+         this.tipo = tipo;
+         this.madeira = madeira;
+         this.preço = preço;
+         }
+
+  public string getNumeroSerie(){
+      return numeroSerie;
+      }
+  public void setNumeroSerie(String numeroSerie){
+      this.numeroSerie = numeroSerie;
+      }
+  public string getFabricante(){
+      return fabricante;
+      }
+  public void setFabricante(String fabricante){
+      this.fabricante = fabricante;
+      }
+  public string getModelo(){
+      return modelo;
+      }
+  public void setModelo(String modelo){
+      this.modelo = modelo;
+      {
+  public string getTipo(){
+      return tipo;
+      }
+  public void setTipo(String tipo){
+      this.tipo = tipo;
+      }
+  public string getMadeira(){
+      return madeira;
+      }
+  public void setMadeira(String madeira){
+      this.madeira = madeira;
+      }
+  public double getPreço(){
+      return preço;
+      }
+  public void setPreço(double preço){
+      this.preço = preço;
+      }
+////o método main() é o método principal da classe
+
+  public static void main(String[] args){
+
+  ////instanciamos um objeto chamado "minhaGuitarra" que será do tipo "Guitarra"
+  ////Os valores passados são usados pelo construtor da classe para criar o objeto
+
+      Guitarra minhaGuitarra = new Guitarra("0605710", "fender", "telecaster", "eletrica", "mogno", 2000);
+
+  ////Testando os dados da classe, imprimindo a saída simples no terminal
+
+  system.out.println(minhaGuitarra.getNumeroSerie());
+  system.out.println(minhaGuitarra.getMadeira());
+  system.out.println(minhaGuitarra.getPreço());
+  system.out.println(minhaGuitarra.getTipo());
+  }
+
+ }
